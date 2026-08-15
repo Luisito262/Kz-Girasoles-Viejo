b1ba013d2ccf6995a462aab3dbf564f6f6125e02
Luisito262 <151080623+Luisito262@users.noreply.github.com>
2026-08-15 16:18:43 -0500

    Menu Kz Viejo
    Respaldo

---
+++ public_html/js/data.js
@@ -0,0 +1,741 @@
+restaurante={
+    comida: [
+        {
+        grupo: "ENTRADAS",
+        imagen: "",
+        productos: [
+            {
+            nombre: "PICADA DE ROBALO",
+            precio: 75000,
+            descripcion: "",
+            },            
+            {
+            nombre: "PICADA DE ROBALO Y HUEVAS",
+            precio: 75000,
+            descripcion: "",
+            },                        
+            {
+            nombre: "PICADA MIXTA ROBALO Y BASA",
+            precio: 70000,
+            descripcion: "",
+            },                        
+            {
+            nombre: "PICADA MIXTA H + FILETE DE BASA",
+            precio: 65000,
+            descripcion: "",
+            },
+            {
+            nombre: "PICADA DE HUEVAS",
+            precio: 60000,
+            descripcion: "",
+            },            
+            {
+            nombre: "PICADA MIXTA POLLO Y CARNE",
+            precio: 60000,
+            descripcion: "",
+            },
+            {
+            nombre: "PICADA DE FILETE BASA",
+            precio: 55000,
+            descripcion: "",
+            },            
+            {
+            nombre: "COCTEL DE CAMARON",
+            precio: 28000,
+            descripcion: "",
+            },
+            {
+            nombre: "CEVICHE DE CAMARON",
+            precio: 28000,
+            descripcion: "",
+            },
+            {
+            nombre: "SALSA DE CAMARON",
+            precio: 28000,
+            descripcion: "",
+            },
+            {
+            nombre: "CAMARON AL AJILLO",
+            precio: 28000,
+            descripcion: "",
+            }            
+            ]
+        },        
+        {
+        grupo: "ADICIONALES",
+        imagen: "",
+        productos: [
+            {				
+            nombre: "PORCION DE ENSALADA",
+            precio: 4000,
+            descripcion: "",
+            },            		
+            {
+            nombre: "ARROZ BLANCO",
+            precio: 4000,
+            descripcion: "",
+            },
+            {				
+            nombre: "ARROZ EN COCO",
+            precio: 5000,
+            descripcion: "",
+            },
+            {				
+            nombre: "SOPAS",
+            precio: 10000,
+            descripcion: "",
+            },
+            {
+            nombre: "PATACON CON AHOGAO",
+            precio: 15000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PATACON CON QUESO",
+            precio: 15000,
+            descripcion: "",
+            },		
+            {
+            nombre: "DESAYUNOS CON HUEVO",
+            precio: 15000,
+            descripcion: "",
+            }            
+            ]
+        },
+        {
+        grupo: "ESPECIALES",
+        imagen: "",
+        productos: [
+            {				
+            nombre: "ARROZ CON POLLO",
+            precio: 35000,
+            descripcion: "",
+            },
+            {
+            nombre: "CARNE A LA PLANCHA",
+            precio: 34000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PECHUGA A LA PLANCHA",
+            precio: 34000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PECHUGA EN S. CHAMPINON",
+            precio: 40000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PECHUGA GRATINADA",
+            precio: 38000,
+            descripcion: "",
+            },
+            {
+            nombre: "ARROZ DE MARISCOS Y MEJILLON",
+            precio: 47000,
+            descripcion: "",
+            },
+            {				
+            nombre: "ARROZ A LA MARINERA",
+            precio: 48000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PASTAS A LA MARINERA",
+            precio: 48000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PASTAS EN SALSA DE CAMARON",
+            precio: 48000,
+            descripcion: "",
+            },		
+            {
+            nombre: "ARROZ CON CAMARON",
+            precio: 48000,
+            descripcion: "",
+            },
+            {				
+            nombre: "ARROZ CAMARÓN Y MEJILLON",
+            precio: 50000,
+            descripcion: "",
+            }            
+            ]
+        },
+        {
+        grupo: "PLATOS FUERTES",
+        imagen: "",
+        productos: [            
+            {
+            nombre: "ROBALO MEDIANO AL CAMARON",
+            precio: 75000,
+            descripcion: "",
+            },
+            {
+            nombre: "ROBALO GRANDE",
+            precio: 75000,
+            descripcion: "",
+            },
+            {
+            nombre: "LEBRANCHE GRANDE",
+            precio: 70000,
+            descripcion: "",
+            },            
+            {
+            nombre: "LEBRANCRE MEDIANO AL CAMARON",
+            precio: 70000,
+            descripcion: "",
+            },
+            {
+            nombre: "ROBALO AL AJILLO",
+            precio: 68000,
+            descripcion: "",
+            },
+            {
+            nombre: "ROBALO MEDIANO",
+            precio: 65000,
+            descripcion: "",
+            },                         
+            {
+            nombre: "LEBRANCHE MEDIANO",
+            precio: 60000,
+            descripcion: "",
+            },           
+            {
+            nombre: "FILETE DE ROBALO GRATINADO",
+            precio: 55000,
+            descripcion: "",
+            },                        
+            {
+            nombre: "CAZUELA DE LA CASA",
+            precio: 60000,
+            descripcion: "",
+            },            
+            {
+            nombre: "COJINÚA",
+            precio: 55000,
+            descripcion: "",
+            },
+            {
+            nombre: "FILETE DE ROBALO AL CAMARON",
+            precio: 55000,
+            descripcion: "",
+            },
+            {
+            nombre: "CAZUELA DE MARISCOS",
+            precio: 55000,
+            descripcion: "",
+            },            
+            {
+            nombre: "FILETE DE ROBALO AL AJILLO",
+            precio: 50000,
+            descripcion: "",
+            },
+            {
+            nombre: "COJINUA MEDIANA",
+            precio: 45000,
+            descripcion: "",
+            },            
+            {
+            nombre: "MOJARRA AL CAMARON",
+            precio: 45000,
+            descripcion: "",
+            },            
+            {
+            nombre: "FILETE DE ROBALO",
+            precio: 45000,
+            descripcion: "",
+            },
+            {
+            nombre: "FILETE DE BASA GRATINADO",
+            precio: 42000,
+            descripcion: "",
+            },
+            {
+            nombre: "MOJARRA EN SALA DE COCO",
+            precio: 42000,
+            descripcion: "",
+            },                        
+            {
+            nombre: "FILETE DE BASA EN AJILLO",
+            precio: 40000,
+            descripcion: "",
+            },
+            {
+            nombre: "MOJARRA FRITA SUDADA",
+            precio: 40000,
+            descripcion: "",
+            },
+            {
+            nombre: "MOJARRA SUDADA",
+            precio: 38000,
+            descripcion: "",
+            },                        
+            {
+            nombre: "MOJARRA ROJA",
+            precio: 40000,
+            descripcion: "",
+            },            
+            {
+            nombre: "SIERRA EN POSTA",
+            precio: 37000,
+            descripcion: "",
+            },
+            {
+            nombre: "FILETE BASA",
+            precio: 35000,
+            descripcion: "",
+            },
+            {
+            nombre: "MOJARRA LORA",
+            precio: 37000,
+            descripcion: "",
+            },            
+            {
+            nombre: "PARGO ROJO (LO QUE PESE)",
+            precio: null,
+            descripcion: "",
+            }
+            ]
+        },
+        {
+        grupo: "LUNES A VIERNES (EXCEPTO FESTIVOS)",
+        imagen: "",
+        productos: [
+            {				
+            nombre: "MOJARRA LORA",
+            precio: 31000,
+            descripcion: "",
+            },		
+            {
+            nombre: "MOJARRA ROJA",
+            precio: 33000,
+            descripcion: "",
+            }
+            ]
+        }        
+
+    ],
+    bebida: [
+        {
+        grupo: "LIMONADAS",
+        imagen: "img/limonada.png",
+        add: "",
+        productos: [
+            {
+            nombre: "MARGARITA NATURAL",
+            precio: 20000,
+            descripcion: ""
+            },
+            {
+            nombre: "MARGARITA DE FRESA",
+            precio: 20000,
+            descripcion: ""
+            },
+            {
+            nombre: "MARGARITA DE MARACUYA",
+            precio: 20000,
+            descripcion: ""
+            },
+            {
+            nombre: "LUNA AZUL",
+            precio: 20000,
+            descripcion: ""
+            },
+            {
+            nombre: "PIÑA COLADA",
+            precio: 20000,
+            descripcion: ""
+            },
+            {
+            nombre: "LIMONADA DE COCO",
+            precio: 16000,
+            descripcion: ""
+            },
+            {
+            nombre: "LIMONADA CEREZADA",
+            precio: 14000,
+            descripcion: ""
+            },
+            {
+            nombre: "LIMONADA MANGO BICHE",
+            precio: 14000,
+            descripcion: ""
+            },
+            {
+            nombre: "LIMONADA NATURAL",
+            precio: 10000,
+            descripcion: ""
+            },
+            {
+            nombre: "VASOS MICHELADOS",
+            precio: 5000,
+            descripcion: ""
+            }
+            ]
+        },
+        {
+        grupo: "SODAS",
+        imagen: "img/sodatrans.png",
+        add: "img/",
+        productos: [
+            {
+            nombre: "FUZE TÉ",
+            precio: 5000,
+            descripcion: ""
+            },                        
+            {
+            nombre: "SODA GRAN BRETAÑA",
+            precio: 6000,
+            descripcion: ""
+            },
+            {
+            nombre: "SODA HATSU",
+            precio: 9000,
+            descripcion: ""
+            }                        
+            ]
+        },
+        {
+        grupo: "BEBIDAS CALIENTES",
+        imagen: "img/cafe.png",
+        add: "img/",
+        productos: [
+            {
+            nombre: "CAFÉ TINTO",
+            precio: 3000,
+            descripcion: ""
+            },
+            {
+            nombre: "CAFÉ CON LECHE",
+            precio: 4500,
+            descripcion: ""
+            },
+            {
+            nombre: "MILO CALIENTE",
+            precio: 7000,
+            descripcion: ""
+            }
+            ]
+        },
+        {
+        grupo: "ENERGIZANTES, HIDRATANTES Y GASEOSAS",
+        imagen: "img/detodito.png",
+        add: "img/",
+        productos: [
+            {
+            nombre: "COCA COLA 1.5",
+            precio: 13000,
+            descripcion: ""
+            },
+            {
+            nombre: "REDBULL",
+            precio: 12000,
+            descripcion: ""
+            },
+            {
+            nombre: "MONSTERS",
+            precio: 12000,
+            descripcion: ""
+            },            
+            {
+            nombre: "GATORADE",
+            precio: 8000,
+            descripcion: ""
+            },
+            {
+            nombre: "AGUA MINERAL MANANTIAL 500ML",
+            precio: 7000,
+            descripcion: ""
+            },
+            {
+            nombre: "POWERADE",
+            precio: 7000,
+            descripcion: ""
+            },
+            {
+            nombre: "JUGO HÍT",
+            precio: 6000,
+            descripcion: ""
+            },
+            {
+            nombre: "AGUA CON GAS 600 ML",
+            precio: 6000,
+            descripcion: ""
+            },                        
+            {
+            nombre: "GINGER",
+            precio: 6000,
+            descripcion: ""
+            },
+            {
+            nombre: "GASEOSA PERSONAL",
+            precio: 6000,
+            descripcion: ""
+            },
+            {
+            nombre: "PONY 330 ML",
+            precio: 5500,
+            descripcion: ""
+            },
+	        {
+            nombre: "VIVE 100 - SPARTAN",
+            precio: 5500,
+            descripcion: ""
+            },
+            {
+            nombre: "AGUA DE MANZANA",
+            precio: 5000,
+            descripcion: ""
+            },
+            {
+            nombre: "AGUA BRISA",
+            precio: 5000,
+            descripcion: ""
+            },                                                
+            {
+            nombre: "AGUA GENESIS GLACIAL 600ML",
+            precio: 4000,
+            descripcion: ""
+            }
+            ]
+        },
+        {
+        grupo: "CERVEZAS Y LICORES",
+        imagen: "img/cerveza.png",
+        add: "img/",
+        productos: [
+            {
+            nombre: "SMIRNOFF",
+            precio: 14000,
+            descripcion: ""
+            },
+            {
+            nombre: "STELLA ARTOIS",
+            precio: 10000,
+            descripcion: ""
+            },
+            {
+            nombre: "CORONA",
+            precio: 10000,
+            descripcion: ""
+            },
+            {
+            nombre: "LIKE",
+            precio: 8000,
+            descripcion: ""
+            },
+            {
+            nombre: "CLUB COLOMBIA",
+            precio: 7000,
+            descripcion: ""
+            },
+            {
+            nombre: "CORONITA",
+            precio: 7000,
+            descripcion: ""
+            },
+            {
+            nombre: "CUATES",
+            precio: 8000,
+            descripcion: ""
+            },
+            {
+            nombre: "BUDWEISER",
+            precio: 7000,
+            descripcion: ""
+            },            
+            {
+            nombre: "AGUILA LIGTH",
+            precio: 6000,
+            descripcion: ""
+            },            
+            {
+            nombre: "AGUILA",
+            precio: 5500,
+            descripcion: ""
+            },
+            {
+            nombre: "POKER",
+            precio: 5500,
+            descripcion: ""
+            },
+            {
+            nombre: "COSTEÑA",
+            precio: 5500,
+            descripcion: ""
+            },
+            {
+            nombre: "COLA Y POLA",
+            precio: 5500,
+            descripcion: ""
+            },
+            {
+            nombre: "COSTEÑITA",
+            precio: 4500,
+            descripcion: ""
+            }
+            ]				
+        },                
+        {
+        grupo: "CÓCTELES REFRESCANTES",
+        imagen: "img/coctelTrans1.png",
+        add: "img/descuento2.png",
+        productos: [
+            {
+            nombre: "MARGARIMEX",
+            precio: 25000,
+            descripcion: "(tequila, triple sec, limón) 10% vol. alcohol"
+            },
+            {
+            nombre: "BICHOTA",
+            precio: 25000,
+            descripcion: "(champagne rose, frutos rojos) 10% vol. alcohol"
+            },
+            {
+            nombre: "MOJICUBANO",
+            precio: 25000,
+            descripcion: "(Ron, limón, menta) 10% vol. Alcohol"
+            },
+            {
+            nombre: "BELLACO",
+            precio: 25000,
+            descripcion: "(ginebra, lulo, vodka) 10% vol. Alcohol"
+            },
+            {
+            nombre: "ASICALAO",
+            precio: 25000,
+            descripcion: "(coñac, arándanos, sandía) 10% vol. Alcohol"
+            },
+            {
+            nombre: "BÁRBARO",
+            precio: 25000,
+            descripcion: "(whisky, walker, Ron total, kola roman) 10% vol. alcohol"
+            },
+            {
+            nombre: "PERREO",
+            precio: 25000,
+            descripcion: "(whisky, sandía, tónica) 10% vol. Alcohol"
+            },
+            {
+            nombre: "BENGALLAO",
+            precio: 25000,
+            descripcion: "(whisky, bon bon bum) 10% vol. Alcohol"
+            }
+            ]
+        },
+        {
+        grupo: "CÓCTELES CREMOSOS",
+        imagen: "img/coctelTrans2.png",
+        add: "img/descuento2.png",
+        productos: [
+            {
+            nombre: "MARACUSEX",
+            precio: 27000,
+            descripcion: "(maracuyá, ron blanco) 15% vol. Alcohol"
+            },
+            {
+            nombre: "BAILEYS",
+            precio: 27000,
+            descripcion: "(crema de whisky) 17% vol. alcohol"
+            },
+            {
+            nombre: "PIÑA COLADA",
+            precio: 27000,
+            descripcion: "(piña, coco, ron blanco) 12% vol. alcohol"
+            },
+            {
+            nombre: "COCOCREAM",
+            precio: 27000,
+            descripcion: "(coco, ron blanco) 15% vol. Alcohol"
+            },
+            {
+            nombre: "COFFE",
+            precio: 27000,
+            descripcion: "(licor de café) 15% vol. Alcohol"
+            },
+            {
+            nombre: "ROSSE",
+            precio: 27000,
+            descripcion: "(fresa, tequila) 15% vol. Alcohol"
+            }
+            ]
+        }
+
+    ],
+    kiosco: [
+        {        
+        grupo: "VITRINA",
+        imagen: "",
+        add: "img/mekatos.png",
+        productos: [            
+            {
+            nombre: "ACETAMINOFEN (PARACETAMOL)",
+            precio: 800,
+            descripcion: "",
+            },
+            {
+            nombre: "CHICLE TRIDENT",
+            precio: 800,
+            descripcion: "",
+            },
+            {				
+            nombre: "BON BON BUM",
+            precio: 1000,
+            descripcion: "",
+            },            
+            {				
+            nombre: "CIGARRILLOS LUCKY STRIKE",
+            precio: 1500,
+            descripcion: "",
+            },
+            {				
+            nombre: "TOALLAS HIGIENICAS",
+            precio: 1500,
+            descripcion: "",
+            },
+            {				
+            nombre: "MECHERAS",
+            precio: 2000,
+            descripcion: "",
+            },
+            {
+            nombre: "GALLETAS (OREO, CHOKIES)",
+            precio: 2500,
+            descripcion: "",
+            },
+            {				
+            nombre: "DORITOS",
+            precio: 4000,
+            descripcion: "",
+            },
+            {				
+            nombre: "PAPAS MARGARITA",
+            precio: 4000,
+            descripcion: "",
+            },
+            {				
+            nombre: "BUSCAPINA",
+            precio: 4000,
+            descripcion: "",
+            },
+            {				
+            nombre: "HALLS",
+            precio: 4000,
+            descripcion: "",
+            },
+            {				
+            nombre: "DETODITOS",
+            precio: 6000,
+            descripcion: "",
+            },
+            {				
+            nombre: "BONFIEST",
+            precio: 6000,
+            descripcion: "",
+            }            
+            ]
+        }
+    ]
+};
